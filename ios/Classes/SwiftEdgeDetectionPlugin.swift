f9ec3db3009172eacd9e03072d6dfbc40f14f027
Rodrigo Ceh <ceh.rodrigo@gmail.com>
2026-02-18 17:46:42 -0600

    fix iOS: scanner opens with scene lifecycle

--- ios/Classes/SwiftEdgeDetectionPlugin.swift
+++ ios/Classes/SwiftEdgeDetectionPlugin.swift
@@ -3,36 +3,50 @@ import UIKit
 import WeScan
 
 public class SwiftEdgeDetectionPlugin: NSObject, FlutterPlugin, UIApplicationDelegate {
-    
+
+    private static var keyWindow: UIWindow? {
+        if #available(iOS 13.0, *) {
+            return UIApplication.shared.connectedScenes
+                .compactMap { $0 as? UIWindowScene }
+                .flatMap { $0.windows }
+                .first { $0.isKeyWindow }
+        }
+        return UIApplication.shared.delegate?.window ?? nil
+    }
+
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "edge_detection", binaryMessenger: registrar.messenger())
         let instance = SwiftEdgeDetectionPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
         registrar.addApplicationDelegate(instance)
     }
-    
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         let args = call.arguments as! Dictionary<String, Any>
         let saveTo = args["save_to"] as! String
         let canUseGallery = args["can_use_gallery"] as? Bool ?? false
 
-        if (call.method == "edge_detect")
-        {
-            if let viewController = UIApplication.shared.delegate?.window??.rootViewController as? FlutterViewController {
-                let destinationViewController = HomeViewController()
-                destinationViewController.setParams(saveTo: saveTo, canUseGallery: canUseGallery)
-                destinationViewController._result = result
-                viewController.present(destinationViewController,animated: true,completion: nil);
+        if (call.method == "edge_detect") {
+            let window = UIApplication.shared.delegate?.window ?? Self.keyWindow
+            guard let viewController = (window ?? Self.keyWindow)?.rootViewController as? FlutterViewController else {
+                result(FlutterError(code: "NO_VIEW_CONTROLLER", message: "No root view controller available.", details: nil))
+                return
             }
+            let destinationViewController = HomeViewController()
+            destinationViewController.setParams(saveTo: saveTo, canUseGallery: canUseGallery)
+            destinationViewController._result = result
+            viewController.present(destinationViewController, animated: true, completion: nil)
         }
-        if (call.method == "edge_detect_gallery")
-        {
-            if let viewController = UIApplication.shared.delegate?.window??.rootViewController as? FlutterViewController {
-                let destinationViewController = HomeViewController()
-                destinationViewController.setParams(saveTo: saveTo, canUseGallery: canUseGallery)
-                destinationViewController._result = result
-                destinationViewController.selectPhoto();
+        if (call.method == "edge_detect_gallery") {
+            let window = UIApplication.shared.delegate?.window ?? Self.keyWindow
+            guard let viewController = (window ?? Self.keyWindow)?.rootViewController as? FlutterViewController else {
+                result(FlutterError(code: "NO_VIEW_CONTROLLER", message: "No root view controller available.", details: nil))
+                return
             }
+            let destinationViewController = HomeViewController()
+            destinationViewController.setParams(saveTo: saveTo, canUseGallery: canUseGallery)
+            destinationViewController._result = result
+            destinationViewController.selectPhoto()
         }
     }
 }
